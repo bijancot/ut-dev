8ed469e9cf1cdfc85b4535bb884f3a713d1c9f5f
Zidan Permana <zidanepe@gmail.com>
2021-02-22 12:21:04 +0700

    user.php

--- application/controllers/api/User.php
+++ application/controllers/api/User.php
@@ -14,6 +14,7 @@ class User extends RestController {
     }
 
     public function login_post(){
+<<<<<<< zidan
         $config = array(
             [
                 'field'     => 'username',
@@ -56,12 +57,34 @@ class User extends RestController {
                 $this->response(['status' => true, 'message' => 'Data berhasil ditemukan' , 'data' => $resLogin[0]], 200);
             }else{
                 $this->response(['status' => false, 'message' => 'Username atau password salah' ], 200);
+=======
+        $datas = $this->input->post();
+        if(!empty($datas['username']) && !empty($datas['password'])){
+            $checkData = $this->db->get_where('USERS', ['USER_USERS' => $datas['username']])->result();
+            if($checkData != null){
+                $resLogin = $this->db->get_where(
+                    'USERS', 
+                    ['USER_USERS' => $datas['username'], 'PASS_USERS' => hash('sha256', md5($datas['password']))]
+                )->result();
+                if($resLogin != null){
+                    $this->db->where('USER_USERS', $datas['username'])->update('USERS', ['LOGIN_USERS' => 1, 'TOKEN_USERS' => $datas['token']]);
+                    $resLogin[0]->LOGIN_USERS = '1';
+                    $resLogin[0]->TOKEN_USERS = $datas['token'];
+                    $this->response(['status' => true, 'message' => 'Data berhasil ditemukan' , 'data' => $resLogin[0]], 200);
+                }else{
+                    $this->response(['status' => false, 'message' => 'Username atau password salah' ], 200);
+                }
+            }else{
+                $this->response(['status' => false, 'message' => 'Data tidak ditemukan' ], 200);
+>>>>>>> main
             }
+        }else{
+            $this->response(['status' => false, 'message' => 'Parameter tidak cocok' ], 200);
         }
-
     }
 
     public function logout_post(){
+<<<<<<< zidan
         $config = array(
             [
                 'field'     => 'idUser',
@@ -79,6 +102,19 @@ class User extends RestController {
         }else{
             $this->db->where('ID_USERS', $data['idUser'])->update('USERS', ['LOGIN_USERS' => 0, 'TOKEN_USERS' => null]);
             $this->response(['status' => true, 'message' => 'Berhasil logout'], 200);
+=======
+        $datas = $this->input->post();
+        if(!empty($datas['idUser'])){
+            $checkData = $this->db->get_where('USERS', ['ID_USERS' => $datas['idUser']])->result();
+            if($checkData != null){
+                $this->db->where('ID_USERS', $datas['idUser'])->update('USERS', ['LOGIN_USERS' => 0, 'TOKEN_USERS' => null]);
+                $this->response(['status' => true, 'message' => 'Berhasil logout'], 200);
+            }else{
+                $this->response(['status' => false, 'message' => 'Id User tidak ditemukan'], 200);
+            }
+        }else{
+            $this->response(['status' => false, 'message' => 'Parameter tidak cocok'], 200); 
+>>>>>>> main
         }
     }
 
